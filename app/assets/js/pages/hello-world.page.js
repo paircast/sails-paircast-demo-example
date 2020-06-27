f466b1edbf383c8a1318ba46d5bf0dfe4f1af8ea
Ian Jennings <ian@meetjennings.com>
2020-06-27 12:37:07 -0500

    Paircast - https://app.paircast.io/c/h61stDVgE/1148336

--- app/assets/js/pages/hello-world.page.js
+++ app/assets/js/pages/hello-world.page.js
@@ -11,6 +11,7 @@ parasails.registerPage('hello-world', {
     me: {
       deep: true,
       handler: function (newValue) {
+        console.log('going')
         Cloud.updateProfile(newValue)
       }
     }
